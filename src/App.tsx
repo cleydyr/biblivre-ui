c3887678a6a6dab8c3309498bd983e00e6b18096
Cleydyr de Albuquerque <cleydyr@gmail.com>
2023-02-04 18:45:53 +0100

    build: add EUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { EuiProvider } from '@elastic/eui';
 import './App.css';
+import '@elastic/eui/dist/eui_theme_light.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EuiProvider colorMode="light">
+
+    </EuiProvider>
   );
+
 }
 
 export default App;
